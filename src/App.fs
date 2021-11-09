c397dabd1023fb5143888e00853e582485bac967
Dan <danre4@gmail.com>
2021-11-10 00:49:08 +0100

    Added syntax highlighter

--- src/App.fs
+++ src/App.fs
@@ -1,14 +1,63 @@
 module App
 
+open Elmish
+open Elmish.React
+open Fable.Core.JsInterop
+open Fable.React
+open Fable.React.Props
+open Feliz
+open Feliz.Bulma
+open Feliz.Bulma.Operators
+open Feliz.Router
+open Fss
 open Browser.Dom
 
-// Mutable variable to count the number of times we clicked the button
-let mutable count = 0
+open SyntaxHighlighterWrapper
 
-// Get a reference to our button and cast the Element to an HTMLButtonElement
-let myButton = document.querySelector(".my-button") :?> Browser.Types.HTMLButtonElement
+let codeSnippet =
+  """
+type Temp =
+    | DegreesC of float
+    | DegreesF of float
 
-// Register our listener
-myButton.onclick <- fun _ ->
-    count <- count + 1
-    myButton.innerText <- sprintf "You clicked: %i time(s)" count
+// Use one of the cases to create one
+let temp1 = DegreesF 98.6
+let temp2 = DegreesC 37.0
+
+// Pattern match on all cases to unpack
+let printTemp = function
+   | DegreesC t -> printfn "%f degC" t
+   | DegreesF t -> printfn "%f degF" t
+  """.Trim() 
+
+let App = FunctionComponent.Of<unit> (fun model ->
+  let state = Hooks.useState model
+
+  Bulma.section [
+    prop.children [
+      Bulma.container [
+        prop.children [
+          Bulma.title "Advent of Code 2021"
+
+          SyntaxHighlighter.input [
+            SyntaxHighlighter.language "fsharp"
+            SyntaxHighlighter.showLineNumbers true
+
+            SyntaxHighlighter.style SyntaxHighlighterWrapper.vs2015
+
+            prop.children [
+              Html.text codeSnippet
+            ]
+          ]
+        ]
+      ]
+    ]
+  ]
+)
+
+let render() =
+  ReactDom.render(
+    App (),
+    document.getElementById("ReactEntryPoint"))
+
+render()
---
+++ src/SyntaxHighlighterWrapper.fs
@@ -0,0 +1,34 @@
+module SyntaxHighlighterWrapper
+
+open Fable.Core.JsInterop
+open Feliz
+
+// https://github.com/react-syntax-highlighter/react-syntax-highlighter
+// https://www.compositional-it.com/news-blog/working-with-react-components-in-fsharp/
+
+(*
+  USAGE EXAMPLE:
+
+  SyntaxHighlighter.input [
+    SyntaxHighlighter.language "fsharp"
+    SyntaxHighlighter.showLineNumbers true
+
+    SyntaxHighlighter.style SyntaxHighlighterWrapper.vs2015
+
+    prop.children [
+      Html.text codeSnippet
+    ]
+  ]
+*)
+
+let syntaxHighlighter : obj = import "default" "react-syntax-highlighter"
+
+let docco : obj = import "docco" "react-syntax-highlighter/dist/esm/styles/hljs"
+let vs2015 : obj = import "vs2015" "react-syntax-highlighter/dist/esm/styles/hljs"
+
+type SyntaxHighlighter =
+  static member inline value (code : string) = prop.custom ("value", code)
+  static member inline showLineNumbers (lineNumbers : bool) = prop.custom ("showLineNumbers", lineNumbers)
+  static member inline language (lang : string) = prop.custom ("language", lang)
+  static member inline style (s : obj) = prop.custom ("style", s)
+  static member inline input props = Interop.reactApi.createElement (syntaxHighlighter, createObj !!props)
